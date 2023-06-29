3a28cf68becba9313cd29fd71ce8a33f26b70a7b
Florian Wesseler <fwesseler@phoenixcontact.com>
2023-06-29 07:43:11 +0200

    switch to ENV-Variables

--- database.py
+++ database.py
@@ -3,8 +3,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
-DB_URL = "postgresql://postgres:postgres@localhost/postgres"
+load_dotenv()
+DB_NAME=os.getenv('DB_NAME')
+DB_USER=os.getenv('DB_USER')
+DB_PASSWORD=os.getenv('DB_PASSWORD')
+DB_HOSTNAME=os.getenv('DB_HOSTNAME')
+
+DB_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}/{DB_NAME}'
 
 engine = create_engine(DB_URL)
 sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
